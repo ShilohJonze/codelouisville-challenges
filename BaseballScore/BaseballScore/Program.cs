62a449aa39a1d148c7a148d80bd4890bf6008b7a
Matthew Jacobs <majacobs@gmail.com>
2017-10-15 22:32:58 -0400

    Added BaseballScore problem

---
+++ BaseballScore/BaseballScore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BaseballScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Batting Averages");
+            Console.WriteLine("--------------------");
+            // TODO: Display the per-player batting averages to three decimal places.
+
+            Console.WriteLine();
+            Console.WriteLine("Team                  1  2  3  4  5  6  7  8  9  Final");
+            Console.WriteLine("------------------------------------------------------");
+            // TODO: Bonus: For each team display the runs per inning and final score.
+
+            Console.ReadLine();
+        }
+    }
+}
