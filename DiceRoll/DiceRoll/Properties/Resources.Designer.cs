4f1d595a1d876bc694b01c388683c6003cd6ca70
Matthew Jacobs <majacobs@gmail.com>
2017-10-15 23:05:27 -0400

    Added bonus solution for DiceRoll

--- DiceRoll/DiceRoll/Properties/Resources.Designer.cs
+++ DiceRoll/DiceRoll/Properties/Resources.Designer.cs
@@ -59,5 +59,14 @@ namespace DiceRoll.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream ShakeAndRollDiceSound {
+            get {
+                return ResourceManager.GetStream("ShakeAndRollDiceSound", resourceCulture);
+            }
+        }
     }
 }
